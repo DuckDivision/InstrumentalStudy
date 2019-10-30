73448a6df96d7dac1791c04f31467666b6291be3
DuckDivision <baranov-matvej@mail.ru>
2019-10-30 11:38:27 +0300

    Added read test

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
 public class Main {
   public static void main(String[] args)
   {
-    Fractional fractional = new Fractional(4, 2);
+    FractionalList list1 = new FractionalList();
+    FractionalList list2 = new FractionalList();
     try {
-      System.out.println(fractional.toString());
+      list1.read("LOCAL FILE PATH");
+      list2.read("LOCAL FILE PATH");
     }
     catch (Exception e)
     {
       System.out.println(e);
     }
-  
+    Polynom polynom = new Polynom(list1);
+    System.out.println(polynom.sum(new Polynom(list2)));
   }
 }
